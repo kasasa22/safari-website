059b616317a3d21acc5db3275cca06f3fcfda852
kasasa22 <kasasatrevor25@gmail.com>
2025-09-26 12:52:12 +0300

    fix: fixed build issues

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,12 +29,5 @@ export default defineNuxtConfig({
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Montserrat:wght@400;500;600;700&display=swap' }
       ]
     }
-  },
-  
-  
-  nitro: {
-    prerender: {
-      routes: ['/sitemap.xml']
-    }
   }
 })
