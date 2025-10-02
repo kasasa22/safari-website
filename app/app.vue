5cdb151902ca0ab3ce314afe3562c33d5ba4a165
kasasa22 <kasasatrevor25@gmail.com>
2025-10-02 15:34:17 +0300

    Update app/app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,8 +1,13 @@
 <template>
   <NuxtLayout>
     <NuxtPage />
+    <Analytics />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+import { Analytics } from '@vercel/analytics/nuxt'
+</script>
 
 <style>
 @import '../assets/css/base.css';
